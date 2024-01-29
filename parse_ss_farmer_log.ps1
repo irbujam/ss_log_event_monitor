16f8cfa57662e5cf6702f87d80de16933236a1f0
irbujam <158004646+irbujam@users.noreply.github.com>
2024-01-29 01:31:37 -0400

    Add files via upload

--- parse_ss_farmer_log.ps1
+++ parse_ss_farmer_log.ps1
@@ -1,12 +1,16 @@
+<#  ------------------------------------------------------------------------------------------------
+	Script location on Github: https://github.com/irbujam/ss_log_event_monitor
+	--------------------------------------------------------------------------------------------- #>
+
 ##header
 $host.UI.RawUI.WindowTitle = "Subspace Log Event Monitor"
 
 ##Definitions
 $bRefreshPage = $true
 $bSuppressWarnings = $false
-$fullPatternArr = @("Single disk farm","Successfully signed reward hash","plotting", "error")
-$noWarnPatternArr = @("Single disk farm","Successfully signed reward hash","plotting")
+$patternArr = @("Single disk farm","Successfully signed reward hash","plotting", "error")
 
+##Prompt for advanced CLI farmer log filer
 Add-Type -AssemblyName System.Windows.Forms
 $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
     InitialDirectory = [Environment]::GetFolderPath('Desktop') 
@@ -14,6 +18,7 @@ $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
 }
 $null = $FileBrowser.ShowDialog()
 $logFileName = $FileBrowser.filename
+
 ##functions
 #Main process
 function main {
@@ -29,12 +34,6 @@ function main {
 			}
 			else {
 				$bSuppressWarnings = $false
-			}
-			if ($bSuppressWarnings) {
-				$patternArr = $noWarnPatternArr
-			}
-			else {
-				$patternArr = $fullPatternArr
 			}
 
 			$allDetailsTextArr = Select-String -Path $logFileName -Pattern "Single disk farm", "Successfully signed reward hash", "plotting", "error"
@@ -56,7 +55,6 @@ function main {
 					$diskInfoStartPos = $allDetailsArrText.IndexOf($diskInfoLabel)
 					$diskInfoEndPos = $allDetailsArrText.IndexOf("}")
 					$diskNumInfo = $allDetailsArrText.SubString($diskInfoStartPos+$diskInfoLabel.Length,$diskInfoEndPos-$diskInfoLabel.Length-$diskInfoStartPos)
-					#Write-Host "diskNumInfo" $diskNumInfo
 					$rewardByDiskCountArr[$diskNumInfo] = $rewardByDiskCountArr[$diskNumInfo] + 1
 				}
 				elseif ($allDetailsArrText.IndexOf("plotting") -ge 0) {
@@ -64,7 +62,6 @@ function main {
 					$diskInfoStartPos = $allDetailsArrText.IndexOf($diskInfoLabel)
 					$diskInfoEndPos = $allDetailsArrText.IndexOf("}")
 					$diskNumInfo = $allDetailsArrText.SubString($diskInfoStartPos+$diskInfoLabel.Length,$diskInfoEndPos-$diskInfoLabel.Length-$diskInfoStartPos)
-					#Write-Host "diskNumInfo" $diskNumInfo
 					if ($allDetailsArrText.IndexOf("Replotting complete") -ge 0) {
 						$plotSizeByDiskCountArr[$diskNumInfo] = "100%"
 					}
@@ -73,18 +70,17 @@ function main {
 						$plotSizeStartPos = $allDetailsArrText.IndexOf($plotSizeInfoLabel)
 						$plotSizeEndPos = $allDetailsArrText.IndexOf("%")
 						$plotSizeInfo = $allDetailsArrText.SubString($plotSizeStartPos+$plotSizeInfoLabel.Length,$plotSizeEndPos-$plotSizeStartPos)
-						#Write-Host "diskNumInfo" $diskNumInfo
 						$plotSizeByDiskCountArr[$diskNumInfo] = $plotSizeInfo
 					}
 				}
 			}
-			Write-Host "------------------" -ForegroundColor yellow
+			Write-Host "------------------------" -ForegroundColor yellow
 			Write-Host "Summary:" -ForegroundColor green
-			Write-Host "------------------" -ForegroundColor yellow
+			Write-Host "------------------------" -ForegroundColor yellow
 			Write-Host "Total Rewards " $rewardCount
-			Write-Host "------------------" -ForegroundColor yellow
+			Write-Host "------------------------" -ForegroundColor yellow
 			Write-Host "Disk#" "Rewards" "Plot Status" -ForegroundColor cyan
-			Write-Host "------------------" -ForegroundColor yellow
+			Write-Host "------------------------" -ForegroundColor yellow
 			for ($arrPos = 0; $arrPos -lt $rewardByDiskCountArr.Count; $arrPos++) {
 				Write-Host $arrPos "   " $rewardByDiskCountArr[$arrPos] "     " $plotSizeByDiskCountArr[$arrPos]
 			}
@@ -103,15 +99,18 @@ function main {
 					$subHeaderText = "Plotting"
 				}
 				elseif ($pattern.IndexOf("error") -ge 0) {
-					$subHeaderText = "Error"
-					$subHeaderColor = "red"
+					if ($bSuppressWarnings -eq $true) {
+						continue
+					}
+					else {
+						$subHeaderText = "Error"
+						$subHeaderColor = "red"
+					}
 				}
-				Write-Host "------------------" -ForegroundColor yellow
+				Write-Host "------------------------" -ForegroundColor yellow
 				Write-Host $subHeaderText " details:" -ForegroundColor $subHeaderColor
-				Write-Host "------------------" -ForegroundColor yellow
-				#$meaningfulText = Select-String -Path $logFileName -Pattern $pattern
+				Write-Host "------------------------" -ForegroundColor yellow
 				$meaningfulText = $allDetailsTextArr | Select-String -Pattern $pattern
-				#Write-Host $meaningfulText
 				$meaningfulText = $meaningfulText -replace "\\", ".."
 				$seperator = ":"
 				$resultTextArr = parseInputStrToArr $meaningfulText $seperator
@@ -123,25 +122,19 @@ function main {
 						break
 					}
 				}
-				#Write-Host $partialText
 				$meaningfulTextArr = $meaningfulText -split $partialText
 				$textArrSize =  $meaningfulTextArr.Length
-				#Write-Host $textArrSize
 				
-				#for ($arrIndex =0;$arrIndex -lt $textArrSize; $arrIndex++)
 				$bDiskInfoMatchFound = $false
 				$diskInfoHoldArr = [System.Collections.ArrayList]@()
 				for ($arrIndex =$textArrSize-1;$arrIndex -ge 0; $arrIndex--)
 				{
 					if ($meaningfulTextArr[$arrIndex] -ne "") {
-						#Write-Host "array element: "  $meaningfulTextArr[$arrIndex]
 						$dispText = parseInputStr $meaningfulTextArr[$arrIndex] ":"
-						#write-host "subHeaderText: " $subHeaderText
 						if ($subHeaderText -eq "Plotting") {
 							$diskInfoLabel = "{disk_farm_index="
 							$diskInfoStartPos = $dispText.IndexOf($diskInfoLabel)
 							$diskInfoHold = $dispText.SubString($diskInfoStartPos,$diskInfoLabel.Length+2)
-							#write-host "diskInfoHold: " $diskInfoHold
 							$bDiskInfoMatchFound = $false
 							foreach($disk in $diskInfoHoldArr)
 							{
@@ -151,7 +144,7 @@ function main {
 								}
 							}							
 							if ($bDiskInfoMatchFound -eq $false) {
-								$diskInfoHoldArr.Add($diskInfoHold)
+								$tempArrId = $diskInfoHoldArr.Add($diskInfoHold)
 							}
 						}
 						if ($bDiskInfoMatchFound -eq $false) {
@@ -188,31 +181,19 @@ function main {
 }
 
 function parseInputStr([string]$ioSourceText, [string]$delimiter){
-	#Write-Host $ioSourceText
-	$i = $ioSourceText.IndexOf($delimiter)             # get the last separator
-	#$i = $ioSourceText.IndexOf($delimiter, $i + 1)     # get the second to last separator, also the end of the column we are interested in
-	#$i = $ioSourceText.IndexOf($delimiter, $i + 1)     # get the third to last separator, also the end of the column we are interested in
-    #$j = $ioSourceText.LastIndexOf($delimiter, $i - 1)     # get the separator before the column we want
-    #$j++                                    # more forward past the separator
+	$i = $ioSourceText.IndexOf($delimiter)
 	$textPart = $ioSourceText.SubString($i+1,$ioSourceText.Length-$i-1)
 	return $textPart
 }
 function parseInputStrToArr([string]$ioSourceText, [string]$delimiter){
-	#Write-Host $ioSourceText
 	$i = $ioSourceText.IndexOf($delimiter)             # get the last separator
 	$i = $ioSourceText.IndexOf($delimiter, $i + 1)     # get the second to last separator, also the end of the column we are interested in
-	#$i = $ioSourceText.IndexOf($delimiter, $i + 1)     # get the third to last separator, also the end of the column we are interested in
-    #$j = $ioSourceText.LastIndexOf($delimiter, $i - 1)     # get the separator before the column we want
-    #$j++                                    # more forward past the separator
 	$textPart1 = $ioSourceText.SubString(0,$i+1)
 	$textPart2 = $ioSourceText.SubString($i+1,$ioSourceText.Length-$i-1)
-	#Write-Host $textPart1
-	#Write-Host $textPart2
 	$ioReturnTextArr = [System.Collections.ArrayList]@()
 	$ioTempArr = ($textPart1, $textPart2)
 	foreach($item in $ioTempArr)
 	{
-		#Write-Host $item
 		$ioReturnTextArr.Add($item)
 	}
 	return $ioReturnTextArr
